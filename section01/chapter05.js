52c4c5451e3f63126e4283e1cd5ea0ab502438cc
Kimsu-in <tndls0258@naver.com>
2024-03-08 14:53:30 +0900

    git 연동

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -28;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "김수인";
+let myLocation = "현풍";
+let introduce = myLocation + myName;
+
+let introduceText = `${myName}은 ${myLocation}에 삽니다.`; // 템플릿 리터럴 문법
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다) // 개발자가 넣음
+let empty = null;
+
+// 5. Undefined Type
+let none; // 자동으로 들어감
---
+++ section01/chapter06.js
@@ -0,0 +1,24 @@
+// 1. 묵시적 형 변환
+// 자바스크립트 엔진이 알아서 형 변환 하는것
+
+let num = 10;
+let str = "28";
+
+const result = num + str;
+console.log(result); // 1028 -> 문자열로 바뀜
+
+// 2. 명시적 형 변환
+// 프로그래머 내장함수 등을 이용해서 직접 형 변환을 명시
+
+let str1 = "10";
+let strToNum1 = Number(str1);
+console.log(10 + strToNum1);
+
+let str2 = "10개";
+let strToNum2 = parseInt(str2);
+console.log(strToNum2); // 숫자가 앞쪽으로 가있어야함
+
+// 숫자 -> 문자 형변환
+let num1 = 20;
+let numToStr1 = String(num1);
+console.log(numToStr1 + "입니다.");
